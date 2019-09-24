2ca55e7e29e897c6ad6a4677fe1dc08bfdd8c4ac
kometh0616 <realdr1v3n@gmail.com>
2019-09-24 22:24:52 +0300

    Revert "Initial commit"
    This reverts commit 5cf010b5e457a151435193a812e1efd5db898969.

--- index.js
+++
@@ -1,8 +0,0 @@
-const Bot = require('./src/handler/CrownBot')
-const { prefix, token, ownerID } = require('./config.json')
-
-const bot = new Bot({
-    prefix, token, ownerID
-})
-
-bot.init()
--- src/models/Users.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('users', {
-        userID: {
-            type: DataTypes.STRING,
-            unique: true
-        },
-        username: DataTypes.STRING
-    })
-}
