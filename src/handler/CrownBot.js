7d5afee4c7f6975c5a1f9509f612e553977600a8
Kaleb Klein <klein.jae@gmail.com>
2019-10-13 20:08:21 -0400

    Updated init() to handle unhandled promise rejection exception

--- src/handler/CrownBot.js
+++ src/handler/CrownBot.js
@@ -71,7 +71,12 @@ class CrownBot extends Client {
             .loadModels()
             .configureLogging()
             .login(this.token)
-            .then(() => console.log('Logged in.'))
+            .catch(e => console.log(`An error occurred when attempting to log in. ${e}`))
+            .then(f => {
+                if (f) {
+                    console.log('Logged in.')
+                }
+            })
     }
 }
 
