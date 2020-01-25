165bdd888a3d5c1ecabab3dc57f9356d9bd311c3
kometh0616 <realdr1v3n@gmail.com>
2020-01-25 17:58:38 +0200

    pls don't kill me, Slipper

--- src/handler/CrownBot.js
+++ src/handler/CrownBot.js
@@ -80,6 +80,8 @@ class CrownBot extends Client {
             response.sendStatus(200)
         })
 
+        app.listen(process.env.PORT)
+
         setInterval(() => {
             http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me`)
         }, 280000);
